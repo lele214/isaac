273a6ae3f91e9b35efb77de373fa433499253e07
lele <leo2800200@gmail.com>
2025-09-29 10:47:01 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    # connect to database
+    conn = sqlite3.connect(r'data\items.db')
+    cursor = conn.cursor()
+    
+    # list by item_id
+    cursor.execute("SELECT name, item_id, pickup, quality, effect, type, item_pool FROM items ORDER BY item_id")
+    items = cursor.fetchall()
+    
+    conn.close()
+    
+    return render_template('index.html', items=items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,5 @@
+first active the environment with :
+$ .venv\Scripts\activate
+
+then execute the code with :
+$ flask --app main.py run
