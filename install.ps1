60ddefdd6887e4d8f4143d054dce4743d33f2c6a
lele <leo2800200@gmail.com>
2025-09-30 08:34:16 +0200

    better install

--- install.ps1
+++ install.ps1
@@ -6,3 +6,9 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 Write-Output "---> All ok"
+$Rsp = Read-Host "Want to start the website ? 'y for yes'"
+if ($Rsp -eq "y") {
+    Flask --app .\main.py run
+}else {
+    Write-Output "Then if you want to start the website do Flask --app .\main.py run"
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 for setup the .venv
 $ install.ps1
+
+deactivate for leave the .venv
